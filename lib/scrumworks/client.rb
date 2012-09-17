0f5215a84bd94366d297d381b7ca72702c532c24
Eric Barendt <ebarendt@gmail.com>
2012-09-17 15:19:28 -0700

    A method just for connecting to v2 of the API

--- lib/scrumworks/client.rb
+++ lib/scrumworks/client.rb
@@ -3,6 +3,15 @@ require "scrumworks/session"
 module Scrumworks
   class Client
 
+    # Creates a connection to v2 of the ScrumWorks API.
+    # @param [String] root_url the root of the SW installation. ex: http://localhost:8080
+    # @param [String] username the username to use for authentication
+    # @param [String] password the password to use for authentication
+    # @return [Scrumworks::Client] a client configured for API2
+    def self.api2(root_url, username, password)
+      Scrumworks::Client.new("#{root_url}/scrumworks-api/api2/scrumworks", username, password)
+    end
+
     # Creates a new client connection to the given SOAP endpoint.
     # @param [String] endpoint_url the ScrumWorks API endpoint
     # @param [String] username the username with which to authenticate
